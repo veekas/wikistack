a86682f922b1432e0ea94a669b759bf803e4c86a
NicholasGalarza <nicogala93@gmail.com>
2017-09-25 10:02:29 -0400

    merged

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
 app.listen(3000, function() {
   console.log("Listening on port ", 3000);
 });
